05174b8b35887087ea19487847ce4b2b41df014b
DanLucena <daniel.lucena@funcionarios.automatizai.com.br>
2021-08-16 19:19:34 -0300

    Adding efects to the draggable button

--- frontend/src/pages/Home/components/Hero/index.tsx
+++ frontend/src/pages/Home/components/Hero/index.tsx
@@ -1,21 +1,43 @@
 import './style.scss';
 import Particles from 'react-particles-js';
-import { useRef } from "react";
-import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import { motion, useAnimation, useMotionValue, useTransform } from "framer-motion";
 import { FiArrowRight } from 'react-icons/fi';
+import { IoIosArrowDown } from 'react-icons/io';
 
 export function Hero() {
   const constraintsRef = useRef(null);
+  const x = useMotionValue(0);
+  const xInput = [0, 100];
+  const background = useTransform(x, xInput, [
+    "linear-gradient(180deg, #6C63FF 0%, #6C63FF 100%)",
+    "linear-gradient(180deg, #B5B0FF 0%, #B5B0FF 100%)",
+  ]);
+  const [arrowMovement, serArrowMovement] = useState(0);
+
+  const controls = useAnimation();
+
+  function handleDragEnd(_: any, info: any) {
+    const offset = info.offset.x;
+
+    if (offset > 72) {
+      controls.start({ x: 152, opacity: 1, transition: { duration: 0.3 } });
+    } else {
+      controls.start({ x: 0, opacity: 1, transition: { duration: 0.5 } });
+    }
+  }
+
+
   return (
     <div className="hero-container">
       <div className="text-hero">
         <h1 className="first-title">Control your activities and your</h1>
         <h1 className="sub-title">SMARTHOME</h1>
         <p className="text">If you want to have more control over your routine, your activities or the equipment in your smart<br /> home, this is the system you are looking for.</p>
-        <motion.div className="container" ref={constraintsRef} >
+        <motion.div className="container" ref={constraintsRef} style={{ background }}>
           <p>GET START</p>
-          <motion.div className="item" drag="x" dragConstraints={{ left: 0, right: 152 }} >
-            <FiArrowRight color="#6C63FF" size="1.2rem" />
+          <motion.div className="item" drag="x" dragConstraints={{ left: 0, right: 152 }} style={{ x }} onDrag={(event, info) => { serArrowMovement(info.offset.x) }} onDragEnd={handleDragEnd} whileTap={{ cursor: 'grabbing' }} animate={controls}>
+            <FiArrowRight color={arrowMovement > 50 ? "#B5B0FF" : "#6C63FF"} size="1.2rem" />
           </motion.div>
         </motion.div>
       </div>
@@ -23,7 +45,7 @@ export function Hero() {
         params={{
           particles: {
             "number": {
-              "value": 180
+              "value": 150
             },
             line_linked: {
               shadow: {
@@ -40,6 +62,9 @@ export function Hero() {
           width: '100%',
         }}
       />
+      <div className="container-arrow">
+        <IoIosArrowDown className="arrow-icon" size="2.5rem" color="#6C63FF" />
+      </div>
     </div>
   );
 }
